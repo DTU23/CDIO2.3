18816b9a33055b9f1f6d81bef776fa48e049be39
Unknown <spirem@gmail.com>
2017-05-01 17:02:06 +0200

    setup

--- src/main/java/dk/dtu/AppConfig.java
+++ src/main/java/dk/dtu/AppConfig.java
@@ -3,7 +3,7 @@ package dk.dtu;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/*")
+@ApplicationPath("api")
 public class AppConfig extends Application {
 
 }
--- src/main/java/dk/dtu/control/api/v1/OperatorService.java
+++ src/main/java/dk/dtu/control/api/v1/OperatorService.java
@@ -3,7 +3,7 @@ package dk.dtu.control.api.v1;
 import javax.ws.rs.*;
 
 
-@Path("api/v1/operator")
-public class Operator {
+@Path("v1/operator")
+public class OperatorService {
 
 }
