2e46a36be26900c63dee887fafe67c70f6c6ad78
Unknown <spirem@gmail.com>
2017-05-08 14:32:25 +0200

    Removed persistent storage

--- src/main/java/dk/dtu/control/api/v1/OperatorService.java
+++ src/main/java/dk/dtu/control/api/v1/OperatorService.java
@@ -14,21 +14,19 @@ import javax.ws.rs.core.MediaType;
 
 import dk.dtu.model.IDAL;
 import dk.dtu.model.IDAL.DALException;
-import dk.dtu.model.PersistentOperatorDAO;
+import dk.dtu.model.OperatorDAO;
 import dk.dtu.model.Validation;
 import dk.dtu.model.DTO.OperatorDTO;
-import dk.dtu.model.storage.FileStorage;
 
 
 @Path("/v1/operator")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class OperatorService {
 	
-	private IDAL dao = new PersistentOperatorDAO(new FileStorage());
+	private IDAL dao = new OperatorDAO();
 
 	@GET
 	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public OperatorDTO getOperator(@PathParam("id") String oprID) throws DALException {
 		if (Validation.isPositiveInteger(oprID)) {
 			return dao.getOperator(Integer.parseInt(oprID));
@@ -38,16 +36,19 @@ public class OperatorService {
 	}
 	
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<OperatorDTO> getOperatorList() throws DALException {
 		return dao.getOperatorList();
 	}
 
 	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	public boolean createOperator(OperatorDTO opr) throws DALException {
 		return dao.createOperator(opr);
 	}
 	
 	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
 	public boolean updateOperator(OperatorDTO opr) throws DALException {
 		return dao.updateOperator(opr);
 	}
